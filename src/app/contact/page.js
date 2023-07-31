a94a2ea53593fa2da19e1b9f62962ea1c8dc3379
gayan <gayanashoka38@gmail.com>
2023-07-31 09:30:46 +0530

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import BasicButtonGroup from '@/components/navbar'
+
+const Contact = () => {
+  return (
+    <div>Hellow I'm Contact</div>
+  )
+}
+export default Contact;
---
+++ src/app/portfolio/protip.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const protip = () => {
+  return (
+    <div>protip</div>
+  )
+}
+
+export default protip
